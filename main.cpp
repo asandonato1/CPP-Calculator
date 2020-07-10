be9aaafd4b343fda989488476071f703e2408a1a
maze <lockdead456@gmail.com>
2020-07-10 19:05:32 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,31 @@
 
 using namespace std;
 
+int expoFun(int a, int b){ //a = base number, b = power number
+ int result1 = 1;
+ for(int i = 0; i < b; i++){
+    result1 = result1 * a;
+ }
+ return result1;
+
+}
+
+void equalTo(int a, int b){
+
+bool isEqualTo;
+if(a % b == 0){
+    isEqualTo = true;
+    if(isEqualTo){
+        cout << "The numbers are equal\n";
+    }
+}
+else {
+     isEqualTo = false;
+     cout << "The numbers are not equal\n";
+}
+
+}
+
 int main()
 {
     double num1, num2; //numbers to perform op
@@ -11,9 +36,12 @@ int main()
     char op;
     double result;
     int choice;
+    bool calcQuit = false;
     cout << "[1] Regular calculator\n";
     cout << "[2] Compare two numbers\n";
+    cout << "[3] Quit the calculator\n";
     cin >> choice;
+    do{
     if(choice == 1){
     cout << "Enter first number " << endl;
     cin >> num1;
@@ -25,38 +53,43 @@ int main()
 case '+':
     result = num1 + num2;
     cout << result << endl;
+    calcQuit = false;
     break;
 case '-':
     result = num1 - num2;
     cout << result << endl;
+    calcQuit = false;
     break;
 case '*':
     result = num1 * num2;
     cout << result << endl;
+    calcQuit = false;
     break;
 case '/':
     result = num1 / num2;
     cout << result << endl;
+    calcQuit = false;
     break;
+case '^':
+    cout << expoFun(num1, num2);
 default:
-    cout << "invalid input type\n";
+    //cout << "invalid input type\n";
+    calcQuit = true;
     break;
     }
     }
 if(choice == 2){
-    cout << "Input two numbers to compare\n";
     cin >> num3;
     cin >> num4;
-    int compare;
-    if(num3 % num4 == 0){
-        cout << "the two numbers are equal\n";
-    }
-    else if(num3 % num4 != 0){
-        cout << "The two numbers are not equal\n";
-    }
+   equalTo(num3, num4);
+}
+if(choice == 3){
+    calcQuit = true;
+    return 0;
 }
 else{
     return 0;
 }
     return 0;
+} while(!calcQuit);
 }
